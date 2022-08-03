4c5b8093d304663c095ef0dcc83941a93b8da41a
Amaru RDP <amarudin.ad@gmail.com>
2022-08-03 11:03:27 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavbarMenu from "./components/NavbarMenu";
 import About from "./pages/About";
+
 
 function App() {
   return (
