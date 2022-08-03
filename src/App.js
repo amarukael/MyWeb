4ff08775b33c1f2ae4eeabd9d05350a907e4879b
Amaru RDP <amarudin.ad@gmail.com>
2022-08-03 14:56:22 +0000

    add icon

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <Router>
         <NavbarMenu />
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,29 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "../style/Home.css";
 
 function Home() {
   return (
-    <Container className="mt-3">
+    <Container className="mt-5 p-5">
       <Row>
         <Col>
           <div className="ak-hallo">
             <span>Hello I'm</span>
           </div>
-          <div className="mt-2">
-            <span>Akash</span>
+          <div className="mt-3 ak-name">
+            <span>M. Fahmi Amaruddin</span>
           </div>
+          <div className="mt-n1 ak-profession">
+            <span>Web and Android Developer</span>
+          </div>
+          <Container className="mt-4" >
+          <Row>
+            <Col><FontAwesomeIcon icon={faEnvelope} inverse/></Col>
+            <Col className="ak-text">amarudin.ad@gmail.com</Col>
+          </Row>
+          </Container>
         </Col>
       </Row>
     </Container>
