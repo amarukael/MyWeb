b5ff267386c02e337296a08d6ccd59157d269ca8
Amaru RDP <amarudin.ad@gmail.com>
2022-08-03 11:45:21 +0000

    add halo am

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavbarMenu from "./components/NavbarMenu";
 import About from "./pages/About";
-
+import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <div className="App">
+      <Router>
         <NavbarMenu />
+
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../style/Home.css";
 
 function Home() {
   return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
+    <Container className="mt-3">
+      <Row>
+        <Col>
+          <div className="ak-hallo">
+            <span>Hello I'm</span>
+          </div>
+          <div className="mt-2">
+            <span>Akash</span>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Home
+export default Home;
